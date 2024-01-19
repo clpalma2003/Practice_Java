9169e5b6aaaf14bb4ebffec138fd6ea1da024e09
Camila <clpalma2003@gmail.com>
2024-01-19 03:27:17 +0100

    feat:Update final pacman

--- RetosMansias/Pacman.java
+++ RetosMansias/Pacman.java
@@ -1,7 +1,14 @@
-package Practice_Java.RetosMansias;
+package RetosMansias;
+
+import java.util.Random;
 import java.util.Scanner;
 
 class Pacman {
+
+    static Scanner scanner = new Scanner(System.in);
+    static String lastDirection = "RIGHT";
+    static int lives = 1;
+    static int[] ghostPosition = new int[] { 4, 4 };
 
     public static void main(String[] args) {
         int[][] pacmanWorld = {
@@ -23,39 +30,136 @@ class Pacman {
         };
 
         int[] pacmanPosition = new int[] { 1, 1 };
-        boolean isRunning = true;
-        int totalScore = 0;
 
         renderWorld(pacmanWorld, pacmanPosition);
+        runGameLoop(pacmanWorld, pacmanPosition);
 
-        for (int time = 0; time < 100; time++) {
-            pacmanPosition = movePacman(pacmanPosition, pacmanWorld.length, pacmanWorld[0].length, pacmanWorld);
-            totalScore += calculateScore(pacmanWorld, pacmanPosition);
+        scanner.close();
+    }
 
-            System.out.println("Time: " + time + " Segundos");
-            System.out.println("Total Score: " + totalScore);
-            System.out.println(
-                    "Pacman position: " + pacmanPosition[0] + " fila " + " : " + pacmanPosition[1] + " columna");
-            renderWorld(pacmanWorld, pacmanPosition);
+    private static void renderWorld(int[][] world, int[] pacmanPosition) {
+        System.out.println("-".repeat(world[0].length * 3 + 2));
+        for (int row = 0; row < world.length; row++) {
+            for (int column = 0; column < world[row].length; column++) {
+
+                if (row == ghostPosition[0] && column == ghostPosition[1]) {
+                    System.out.print("{} ");
+
+                } else if (row == pacmanPosition[0] && column == pacmanPosition[1]) {
+
+                    String pacmanRepresentation;
+                    switch (lastDirection) {
+                        case "UP":
+                            pacmanRepresentation = " :V";
+                            break;
+                        case "DOWN":
+                            pacmanRepresentation = " :^";
+                            break;
+                        case "LEFT":
+                            pacmanRepresentation = " >:";
+                            break;
+                        case "RIGHT":
+                            pacmanRepresentation = " :<";
+                            break;
+                        default:
+                            pacmanRepresentation = " :<";
+                    }
+                    System.out.print(pacmanRepresentation);
+                } else {
+                    System.out.print(getpacmanWorldSymbol(world[row][column]));
+                }
+            }
+
+            System.out.println();
         }
+        System.out.println("-".repeat(world[0].length * 3 + 2));
+    }
+
+    private static void moveGhostOne(int[][] pacmanWorld) {
+        Random randomMovementOfGosth = new Random();
+
+        boolean ifGhosthmove = false;
+
+        while (!ifGhosthmove) {
+            int direction = randomMovementOfGosth.nextInt(4);
+            int newX = ghostPosition[0];
+            int newY = ghostPosition[1];
+
+            switch (direction) {
+                case 0:
+                    newX--;
+                    break;
+                case 1:
+                    newX++;
+                    break;
+                case 2:
+                    newY--;
+                    break;
+                case 3:
+                    newY++;
+                    break;
+            }
+
+            if (newX >= 0 && newX < pacmanWorld.length && newY >= 0 && newY < pacmanWorld[0].length
+                    && (pacmanWorld[newX][newY] == 0 || pacmanWorld[newX][newY] == 1)) {
+                ghostPosition[0] = newX;
+                ghostPosition[1] = newY;
+                ifGhosthmove = true;
+            }
+        }
+    }
+
+    private static void runGameLoop(int[][] pacmanWorld, int[] pacmanPosition) {
+        boolean hasEnoughLives = true;
+        boolean thereAreStillDots = true;
+        boolean isRunning = hasEnoughLives && thereAreStillDots;
+        int totalScore = 0;
+        int time = 0;
 
         while (isRunning) {
-            renderWorld(pacmanWorld, pacmanPosition);
+            time++;
             pacmanPosition = movePacman(pacmanPosition, pacmanWorld.length, pacmanWorld[0].length, pacmanWorld);
+            moveGhostOne(pacmanWorld);
+            totalScore += calculateScore(pacmanWorld, pacmanPosition);
+            renderWorld(pacmanWorld, pacmanPosition);
+
+            if (checkCollision(pacmanPosition, ghostPosition)) {
+                lives--;
+                if (lives == 0) {
+                    System.out.println("¡Game Over! Has perdido todas tus vidas.");
+                    hasEnoughLives = false;
+                } else if (lives == 2 || lives == 1) {
+                    System.out.println("Haz perdido una vida.");
+                } else {
+                    pacmanPosition[0] = 1;
+                    pacmanPosition[1] = 1;
+                }
+            }
+
+            System.out.println("Time: " + time + " seconds");
+            System.out.println(
+                    "Pacman position: " + pacmanPosition[0] + " fila " + " : " + pacmanPosition[1] + " columna");
+            System.out.println("Total Score: " + totalScore);
+            System.out.println("Total Lives: " + lives);
+
+            thereAreStillDots = checkDots(pacmanWorld, pacmanPosition);
+
+            isRunning = hasEnoughLives && thereAreStillDots;
+
         }
 
     }
 
-    private static int calculateScore(int[][] pacmanWorld, int[] pacmanPosition) {
-        if (pacmanWorld[pacmanPosition[0]][pacmanPosition[1]] == 1) {
-            pacmanWorld[pacmanPosition[0]][pacmanPosition[1]] = 0;
-            return 10;
-        }
-
-        return 0;
+    private static boolean checkCollision(int[] pacmanPosition, int[] ghostPosition) {
+        return pacmanPosition[0] == ghostPosition[0] && pacmanPosition[1] == ghostPosition[1];
     }
 
-    static Scanner scanner = new Scanner(System.in);
+    private static String getpacmanWorldSymbol(int pacmanWorldElement) {
+
+        String[] pacmanWorldSymbols = { "   ", " . ", "\u001B[31m" + "***" + "\u001B[0m", "   " };
+
+        return pacmanWorldSymbols[pacmanWorldElement];
+    }
 
     private static int[] movePacman(int[] pacmanPosition, int rows, int columns, int[][] pacmanWorld) {
 
@@ -68,16 +172,26 @@ class Pacman {
 
         if (movementsPacman == 'w' && newX > 0 && pacmanWorld[newX - 1][newY] != 2) {
             newX--;
+            lastDirection = "UP";
         } else if (movementsPacman == 'a' && newY > 0 && pacmanWorld[newX][newY - 1] != 2) {
             newY--;
+            lastDirection = "LEFT";
         } else if (movementsPacman == 's' && newX < rows - 1 && pacmanWorld[newX + 1][newY] != 2) {
             newX++;
+            lastDirection = "DOWN";
         } else if (movementsPacman == 'd' && newY < columns - 1 && pacmanWorld[newX][newY + 1] != 2) {
             newY++;
+            lastDirection = "RIGHT";
         } else {
             System.out.println("Movimiento no válido. El pacman no puede atravesar paredes.");
         }
 
+        int[] newPosition = handleWarpPoints(newX, newY, columns, pacmanWorld);
+        return newPosition;
+
+    }
+
+    private static int[] handleWarpPoints(int newX, int newY, int columns, int[][] pacmanWorld) {
         if (pacmanWorld[newX][newY] == 3) {
             if (newY == 0) {
                 newY = columns - 2;
@@ -85,31 +199,27 @@ class Pacman {
                 newY = 1;
             }
         }
-
         return new int[] { newX, newY };
-
     }
 
-    private static String getpacmanWorldSymbol(int pacmanWorldElement) {
+    private static int calculateScore(int[][] pacmanWorld, int[] pacmanPosition) {
+        if (pacmanWorld[pacmanPosition[0]][pacmanPosition[1]] == 1) {
+            pacmanWorld[pacmanPosition[0]][pacmanPosition[1]] = 0;
+            return 10;
+        }
 
-        String[] pacmanWorldSymbols = { "   ", " . ", "\u001B[31m" + "***" + "\u001B[0m", "   " };
-
-        return pacmanWorldSymbols[pacmanWorldElement];
+        return 0;
     }
 
-    private static void renderWorld(int[][] world, int[] pacmanPosition) {
-        System.out.println("-".repeat(world[0].length * 3 + 2));
-        for (int row = 0; row < world.length; row++) {
-            for (int column = 0; column < world[row].length; column++) {
-                if (row == pacmanPosition[0] && column == pacmanPosition[1]) {
-                    System.out.print(" :v");
-                } else {
-                    System.out.print(getpacmanWorldSymbol(world[row][column]));
+    private static boolean checkDots(int[][] pacmanWorld, int[] pacmanPosition) {
+        for (int[] row : pacmanWorld) {
+            for (int cell : row) {
+                if (cell == 1) {
+                    return true;
                 }
             }
-
-            System.out.println();
         }
-        System.out.println("-".repeat(world[0].length * 3 + 2));
+        return false;
     }
+
 }
